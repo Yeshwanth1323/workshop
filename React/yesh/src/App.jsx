b10f11d94aeb0b887591e7ae89ada829feb4628b
Yeshwanth1323 <yeshwanthreddyganta@gmail.com>
2024-10-27 13:06:48 +0530

    6th morning

--- React/yesh/src/App.jsx
+++ React/yesh/src/App.jsx
@@ -11,8 +11,8 @@ const App = () => {
     const ar = "arru baby"
     return (
     <>
-     <div class="box" id="box2">box1</div>
-     <div class="box" id="box3">box2</div>
+     <div className="box" id="box2">box1</div>
+     <div className="box" id="box3">box2</div>
     <p className="text-red-500">{test}</p>
     <p className="text-blue-500">{ar}</p>
        <Navbar appdata={data}  logo={logodata}></Navbar>
--- React/yesh/src/componets/Navbar.jsx
+++ React/yesh/src/componets/Navbar.jsx
@@ -1,18 +1,27 @@
-import React,{ useState} from 'react'
+import React,{ useEffect, useState} from 'react'
 const Navbar = ({appdata,logo}) =>{
 
     const [numberstate ,setNumberstate] = useState(0)
     const [stringstate,setStringstate] = useState ("a")
     const [arraystate,setArraystate] = useState(["a",false,0])
 
-    console.log(numberstate)
-    console.log(stringstate)
-    console.log(arraystate)
+    //console.log(numberstate)
+   // console.log(stringstate)
+    //console.log(arraystate)
 
-    return(
-        <>
-        {appdata.appname},{logo}
-        </>
+    const [count, setCount] = useState(0);
+    useEffect (()=>{
+        alert('effect rendered')
+        console.log("count is " +count)
+    },[count])
+    return (
+    <>
+
+    <button className="p-2 bg-blue-400 text-cyan-100" onClick={ () => {setCount(count+1)}}>
+        Count {count}
+       </button>
+    </>
     )
 }
+
 export default Navbar
