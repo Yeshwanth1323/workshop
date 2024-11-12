43a5c94bd783e7e8e9b2e823011e5418dd583224
Yeshwanth1323 <yeshwanthreddyganta@gmail.com>
2024-11-12 17:21:53 +0530

    13 day

---
+++ express/project/tail/src/api/api.js
@@ -0,0 +1,23 @@
+
+import axios from 'axios'
+
+
+const API = 'http://localhost:3000'
+
+// ProductEndPonts
+const getProducts = () => axios.get(`${API}/products/all`)
+const addProduct = (product) => axios.post(`${API}/products/add`, product)
+
+
+// OrderEndPonts
+const getOrders = () => axios.get(`${API}/orders/all`)
+
+
+//UserEndPoints
+const getUsers = () => axios.get(`${API}/users/all`)
+
+//AuthEndponts
+const Login = (credentials) => axios.post(`${API}/auth/login`, credentials)
+const Register = (credentials) => axios.post(`${API}/auth/register`, credentials)
+
+export { getProducts, getOrders, getUsers, Login, Register, addProduct }
